838ffd401f9b22da6747ff21c59a80fae0975557
Kot <kotvnn@msn.com>
2025-11-26 03:40:47 +0700

    Features: 1. Fix build with pwa (type minimatch)

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,25 @@
-import type { NextConfig } from 'next';
+import type {NextConfig} from 'next';
+import withPWA from 'next-pwa';
+
+const withPWAConfig = withPWA({
+	dest: 'public',
+	register: true,
+	skipWaiting: true,
+	disable: process.env.NODE_ENV === 'development',
+});
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  output: 'standalone', // cần để chạy tốt trong Docker
-  images: {
-    remotePatterns: [new URL('https://w.ladicdn.com/**')],
-  },
+	output: 'standalone',
+
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'w.ladicdn.com',
+				pathname: '/**',
+			},
+		],
+	},
 };
 
-export default nextConfig;
+export default withPWAConfig(nextConfig as any);
