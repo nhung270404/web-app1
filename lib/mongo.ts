029a35c4d081f8e6d1fd104dfeb89c976b17530f
Kot <kotvnn@msn.com>
2025-11-26 03:19:47 +0700

    Features: 1. Test PWA #2 2. Remove role

--- lib/mongo.ts
+++ lib/mongo.ts
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 if (!process.env['MONGODB_URI']) {
-  throw new Error('Vui lòng đặt biến môi trường MONGODB_URI trong .env.local');
+	throw new Error('Vui lòng đặt biến môi trường MONGODB_URI trong .env.local');
 }
 
 const uri = process.env['MONGODB_URI'];
@@ -9,27 +9,31 @@ const uri = process.env['MONGODB_URI'];
 /**
  * Cached connection for MongoDB.
  */
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-expect-error
 let cached = global.mongoose;
 
 if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null };
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-expect-error
+	cached = global.mongoose = {conn: null, promise: null};
 }
 
 async function dbConnect() {
-  if (cached) {
-    if (cached.conn) {
-      return cached.conn;
-    }
+	if (cached) {
+		if (cached.conn) {
+			return cached.conn;
+		}
 
-    if (!cached.promise) {
-      console.log('Connecting to MongoDB...', uri);
-      cached.promise = mongoose.connect(uri).then((mongoose) => {
-        return mongoose;
-      });
-    }
-    cached.conn = await cached.promise;
-    return cached.conn;
-  }
+		if (!cached.promise) {
+			console.log('Connecting to MongoDB...', uri);
+			cached.promise = mongoose.connect(uri).then((mongoose) => {
+				return mongoose;
+			});
+		}
+		cached.conn = await cached.promise;
+		return cached.conn;
+	}
 }
 
 export default dbConnect;
